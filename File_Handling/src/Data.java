ab60ec8b320628528c25c69f6c5d44abffc4d6ff
Anas Al-Mughamsi <Anas.Mughamsi@gmail.com>
2021-09-22 23:53:04 +0300

    update file_handling

--- File_Handling/src/Data.java
+++
@@ -1,7 +0,0 @@
-public class Data {
-    public int x = 5;
-
-    public Data(int x) { 
-        this.x = x;
-    }
-}
--- File_Handling/src/FileHandling.java
+++ File_Handling/src/FileHandling.java
@@ -1,6 +1,44 @@
+import com.sun.source.tree.WhileLoopTree;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class FileHandling {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        // This code should read, write and append file //
+
+        // first create new file
+        File file = new File("F:\\MyWork\\FullJavaStack\\Practice\\File_Handling\\FileTest.txt");
+        boolean createFile;
+
+        if(file.createNewFile()) {
+            System.out.println("File created");
+        } else {
+            System.out.println("File already exist");
+        }
+
+        // Write content in files
+        FileWriter writer = new FileWriter(file);
+        writer.write("Adding a content here for testing");;
+        writer.close();
+
+        Scanner readFile = new Scanner(file);
+        while (readFile.hasNextLine()) {
+            String data = readFile.nextLine();
+            System.out.println(data);
+        }
+        readFile.close();
+
+//        try {
+//
+//        FileWriter fileWriter = new FileWriter("testing.txt");
+//        } catch (IOException e) {
+//        System.out.println("An Error occurred");
+//        e.printStackTrace();
+//        }
     }
 }
